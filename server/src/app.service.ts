da1a286a99f4ff7ca15773d268354f7770d3cd9f
ALexandrPK <usu99@mail.ru>
2022-10-17 23:32:37 +0300

    add server folder

---
+++ server/src/app.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { Message, Prisma } from '@prisma/client';
+import { MessageUpdatePayload } from 'types';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class AppService {
+  // инициализация сервиса `Prisma`
+  constructor(private readonly prisma: PrismaService) { }
+
+  async getMessages(): Promise<Message[]> {
+    return this.prisma.message.findMany();
+  }
+
+  // удаление всех сообщений - для отладки в процессе разработки
+  async clearMessages(): Promise<Prisma.BatchPayload> {
+    return this.prisma.message.deleteMany();
+  }
+
+  // создание сообщения
+  async createMessage(data: Prisma.MessageCreateInput) {
+    return this.prisma.message.create({ data });
+  }
+
+  // обновление сообщения
+  async updateMessage(payload: MessageUpdatePayload) {
+    const { id, text } = payload;
+    return this.prisma.message.update({ where: { id }, data: { text } });
+  }
+
+  // удаление сообщения
+  async removeMessage(where: Prisma.MessageWhereUniqueInput) {
+    return this.prisma.message.delete({ where });
+  }
+}
